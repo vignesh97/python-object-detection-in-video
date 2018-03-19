072b93aeab4e35dfcdfb1ea690f405ce54f8a271
Vigneshwaran Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2018-03-19 16:10:51 -0400

    status list

--- motion-detector.py
+++ motion-detector.py
@@ -2,23 +2,21 @@ import cv2, time
 
 first_frame = None
 
-
+statusList=[]
 video = cv2.VideoCapture(0)
-
 
 
 a=1
 while True:
     a=a+1
     check, frame = video.read()
-
+    status = 0
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     gray=cv2.GaussianBlur(gray,(21,21),0)
 
     if first_frame is None:
         first_frame=gray
         continue
-
     delta_frame=cv2.absdiff(first_frame,gray)
 
     thresh_frame=cv2.threshold(delta_frame,30,255,cv2.THRESH_BINARY)[1]
@@ -31,6 +29,7 @@ while True:
     for contour in cnts:
         if cv2.contourArea(contour) < 1000:
             continue
+        status=1
         (x,y,w,h)=cv2.boundingRect(contour)
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
 
@@ -43,7 +42,7 @@ while True:
     key =cv2.waitKey(1)
     if key==ord('q'):
         break
-
+    print(status)
 print(a)
 video.release()
 cv2.destroyAllWindows()
