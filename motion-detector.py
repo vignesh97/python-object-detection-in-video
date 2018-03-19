efb180a0a18d5a66a77f88904622fdf6f2615750
Vigneshwaran Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2018-03-19 15:41:15 -0400

    absdiff  to find difference between first frame and current frame

--- motion-detector.py
+++ motion-detector.py
@@ -13,13 +13,13 @@ while True:
     check, frame = video.read()
 
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    gray=cv2.GuassianBlur(gray,(21,21),0)
+    gray=cv2.GaussianBlur(gray,(21,21),0)
 
     if first_frame is None:
         first_frame=gray
         continue
 
-    delta_frame=cv2.absdiff(first_frame.gray)
+    delta_frame=cv2.absdiff(first_frame,gray)
 
     cv2.imshow("Capturing",gray)
     cv2.imshow("Capturing Delta",delta_frame)
