0b926012e0d3907a96a40072c1936647ac94b651
Vigneshwaran Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2018-03-23 19:45:32 -0400

    Column Data Source

--- plotting.py
+++ plotting.py
@@ -1,5 +1,10 @@
 from motion_detector import df
 from bokeh.plotting import figure, show, output_file
+from bokeh.models import HoverTool, ColumnDataSource
+
+
+cds=ColumnDataSource(df)
+
 
 p=figure(x_axis_type='datetime', height=100,width=500,responsive=True,title="Motion graph")
 
@@ -8,8 +13,10 @@ p.yaxis.minor_tick_line_color=None
 p.ygrid[0].ticker.desired_num_ticks=1
 
 
+hover=HoverTool(tooltips=[("Start","@Start_string"),("End","@End_string")])
+p.add_tools(hover)
 
-q=p.quad(left=df["Start"],right=df["End"],bottom=0,top=1,color="green")
+q=p.quad(left=df["Start"],right=df["End"],bottom=0,top=1,color="green",source = cds)
 
 output_file("graph.html")
 show(p)
